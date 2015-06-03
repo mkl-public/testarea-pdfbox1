5f3029ae99a8114fc270e91bd811f3c21bbd6bf4
mkl <mkl@wir-sind-cool.org>
2015-06-04 00:04:05 +0200

    New test DrawSpecialCharacters focusing on the stackoverflow question
    PDFBox unable to write superscripted characters
    http://stackoverflow.com/questions/30619974/pdfbox-unable-to-write-superscripted-characters
    
    testDrawTmSignBroken() represents the observation the OP made,
    testDrawTmSignCustomDraw() is a solution using custom drawing
    operations, and testDrawTmSignLetters() is a solution drawing the
    trademark symbol using smaller letters with text rise.

---
+++ src/test/java/mkl/testarea/pdfbox1/content/DrawSpecialCharacters.java
@@ -0,0 +1,104 @@
+package mkl.testarea.pdfbox1.content;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.pdfbox.exceptions.COSVisitorException;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.edit.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * This test focuses on drawing special characters, something the PDFBox method
+ * {@link PDPageContentStream#drawString(String)} is not really good at-
+ * 
+ * @author mkl
+ */
+public class DrawSpecialCharacters
+{
+    final static File RESULT_FOLDER = new File("target/test-outputs", "content");
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception
+    {
+        RESULT_FOLDER.mkdirs();
+    }
+
+    /**
+     * <a href="http://stackoverflow.com/questions/30619974/pdfbox-unable-to-write-superscripted-characters">
+     * PDFBox unable to write superscripted characters
+     * </a>
+     * <p>
+     * {@link #testDrawTmSignBroken()} represents the observation the OP made,
+     * {@link #testDrawTmSignCustomDraw()} is a solution using custom drawing operations, and
+     * {@link #testDrawTmSignLetters()} is a solution drawing the trademark symbol using smaller letters with text rise.
+     */
+	@Test
+	public void testDrawTmSignBroken() throws IOException, COSVisitorException
+	{
+		PDDocument document = new PDDocument();
+        PDPage page = new PDPage();
+        document.addPage(page);
+        PDPageContentStream contents = new PDPageContentStream(document, page);
+        PDFont font = PDType1Font.HELVETICA_BOLD;
+        contents.beginText();
+        contents.setFont(font, 18);
+        contents.moveTextPositionByAmount(30, 600);
+        contents.drawString("90000039-PREDISOL ® C YELLOW 13 SNDOT™M");
+        contents.endText();
+        contents.close();
+        document.save(new File(RESULT_FOLDER, "TM_naive.pdf"));
+        document.close();
+	}
+
+	@Test
+	public void testDrawTmSignCustomDraw() throws IOException, COSVisitorException
+	{
+		PDDocument document = new PDDocument();
+        PDPage page = new PDPage();
+        document.addPage(page);
+        PDPageContentStream contents = new PDPageContentStream(document, page);
+        PDFont font = PDType1Font.HELVETICA_BOLD;
+        contents.beginText();
+        contents.setFont(font, 18);
+        contents.moveTextPositionByAmount(30, 600);
+        contents.drawString("90000039-PREDISOL ® C YELLOW 13 SNDOT");
+        byte[] commands = "(x) Tj ".getBytes();
+        commands[1] = (byte) 0231;
+        contents.appendRawCommands(commands);
+        contents.drawString("M");
+        contents.endText();
+        contents.close();
+        document.save(new File(RESULT_FOLDER, "TM_customDraw.pdf"));
+        document.close();
+	}
+
+	@Test
+	public void testDrawTmSignLetters() throws IOException, COSVisitorException
+	{
+		PDDocument document = new PDDocument();
+        PDPage page = new PDPage();
+        document.addPage(page);
+        PDPageContentStream contents = new PDPageContentStream(document, page);
+        PDFont font = PDType1Font.HELVETICA_BOLD;
+        contents.beginText();
+        contents.setFont(font, 18);
+        contents.moveTextPositionByAmount(30, 600);
+        contents.drawString("90000039-PREDISOL ® C YELLOW 13 SNDOT");
+        contents.appendRawCommands("\n6 Ts\n".getBytes());
+        contents.setFont(font, 10);
+        contents.drawString("TM");
+        contents.appendRawCommands("\n0 Ts\n".getBytes());
+        contents.setFont(font, 18);
+        contents.drawString("M");
+        contents.endText();
+        contents.close();
+        document.save(new File(RESULT_FOLDER, "TM_letters.pdf"));
+        document.close();
+	}
+
+}
