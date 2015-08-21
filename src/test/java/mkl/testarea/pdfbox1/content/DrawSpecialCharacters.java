602a6e14acf310d7754e32dc652e8a1fb422092b
mkl <mkl@wir-sind-cool.org>
2015-08-21 16:09:42 +0200

    New test DrawSpecialCharacters.testDrawEuro focusing on the stackoverflow question
    PdfBox encode symbol currency euro
    http://stackoverflow.com/questions/22260344/pdfbox-encode-symbol-currency-euro
    
    Three ways of trying to draw a '€' symbol, the naive first one fails.

--- src/test/java/mkl/testarea/pdfbox1/content/DrawSpecialCharacters.java
+++ src/test/java/mkl/testarea/pdfbox1/content/DrawSpecialCharacters.java
@@ -3,6 +3,8 @@ package mkl.testarea.pdfbox1.content;
 import java.io.File;
 import java.io.IOException;
 
+import org.apache.pdfbox.cos.COSName;
+import org.apache.pdfbox.encoding.EncodingManager;
 import org.apache.pdfbox.exceptions.COSVisitorException;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
@@ -29,6 +31,38 @@ public class DrawSpecialCharacters
     }
 
     /**
+     * <a href="http://stackoverflow.com/questions/22260344/pdfbox-encode-symbol-currency-euro">
+     * PdfBox encode symbol currency euro
+     * </a>
+     * <p>
+     * Three ways of trying to draw a '€' symbol, the first one fails.
+     * </p>
+     */
+    @Test
+    public void testDrawEuro() throws IOException, COSVisitorException
+    {
+        PDDocument document = new PDDocument();
+        PDPage page = new PDPage();
+        document.addPage(page);
+        PDPageContentStream contents = new PDPageContentStream(document, page);
+        PDFont font = PDType1Font.HELVETICA_BOLD;
+        contents.beginText();
+        contents.setFont(font, 18);
+        contents.moveTextPositionByAmount(30, 600);
+        contents.drawString("€");
+        contents.moveTextPositionByAmount(0, -30);
+        contents.drawString(String.valueOf(Character.toChars(EncodingManager.INSTANCE.getEncoding(COSName.WIN_ANSI_ENCODING).getCode("Euro"))));
+        contents.moveTextPositionByAmount(0, -30);
+        byte[] commands = "(x) Tj ".getBytes();
+        commands[1] = (byte) 128;
+        contents.appendRawCommands(commands);
+        contents.endText();
+        contents.close();
+        document.save(new File(RESULT_FOLDER, "Euro.pdf"));
+        document.close();
+    }
+
+    /**
      * <a href="http://stackoverflow.com/questions/30619974/pdfbox-unable-to-write-superscripted-characters">
      * PDFBox unable to write superscripted characters
      * </a>
@@ -36,6 +70,7 @@ public class DrawSpecialCharacters
      * {@link #testDrawTmSignBroken()} represents the observation the OP made,
      * {@link #testDrawTmSignCustomDraw()} is a solution using custom drawing operations, and
      * {@link #testDrawTmSignLetters()} is a solution drawing the trademark symbol using smaller letters with text rise.
+     * </p>
      */
 	@Test
 	public void testDrawTmSignBroken() throws IOException, COSVisitorException
