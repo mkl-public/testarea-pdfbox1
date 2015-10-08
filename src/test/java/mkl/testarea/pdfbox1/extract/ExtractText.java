03bdfda2a2308cbcd3c9d5d73354f5a5c2553817
mkl <mkl@wir-sind-cool.org>
2015-10-09 00:46:33 +0200

    New test ExtractText.testExtractLineStartFromSampleFile focusing on the stackoverflow question
    Using PDFBox to get location of line of text
    http://stackoverflow.com/questions/32978179/using-pdfbox-to-get-location-of-line-of-text
    
    This example shows how to extract text with the additional information
    of the x coordinate at the start of line.

--- src/test/java/mkl/testarea/pdfbox1/extract/ExtractText.java
+++ src/test/java/mkl/testarea/pdfbox1/extract/ExtractText.java
@@ -3,10 +3,12 @@ package mkl.testarea.pdfbox1.extract;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 import org.apache.pdfbox.exceptions.COSVisitorException;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.util.PDFTextStripper;
+import org.apache.pdfbox.util.TextPosition;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -61,4 +63,56 @@ public class ExtractText
         //stripper.setSortByPosition(true);
         return stripper.getText(document);
     }
+
+    /**
+     * <a href="http://stackoverflow.com/questions/32978179/using-pdfbox-to-get-location-of-line-of-text">
+     * Using PDFBox to get location of line of text
+     * </a>
+     * <p>
+     * This example shows how to extract text with the additional information of
+     * the x coordinate at the start of line.
+     * </p>
+     */
+    @Test
+    public void testExtractLineStartFromSampleFile() throws COSVisitorException, IOException
+    {
+        try (   InputStream documentStream = getClass().getResourceAsStream("sampleFile.pdf");
+                PDDocument document = PDDocument.load(documentStream))
+        {
+            String normal = extractLineStart(document);
+
+            System.out.println("\n'sampleFile.pdf', extract with line starts:");
+            System.out.println(normal);
+            System.out.println("***********************************");
+        }
+    }
+    
+    String extractLineStart(PDDocument document) throws IOException
+    {
+        PDFTextStripper stripper = new PDFTextStripper()
+        {
+            @Override
+            protected void writeLineSeparator() throws IOException
+            {
+                startOfLine = true;
+                super.writeLineSeparator();
+            }
+
+            @Override
+            protected void writeString(String text, List<TextPosition> textPositions) throws IOException
+            {
+                if (startOfLine)
+                {
+                    TextPosition firstProsition = textPositions.get(0);
+                    writeString(String.format("[%s]", firstProsition.getXDirAdj()));
+                    startOfLine = false;
+                }
+                super.writeString(text, textPositions);
+            }
+            boolean startOfLine = true;
+        };
+        //stripper.setSortByPosition(true);
+        return stripper.getText(document);
+    }
+
 }
