e273ce110e68f32acf84e34d9166a5b5a6ee1518
mkl <mkl@wir-sind-cool.org>
2016-02-16 00:04:01 +0100

    Improved: PrintPaths also outputs width.
    This was necessary as the sample file provided by the OP contains
    vertical lines which are drawn as very short (ca 0.6 units) very thick
    (ca 513 units) horizontal lines...

--- src/main/java/mkl/testarea/pdfbox1/extract/PrintPaths.java
+++ src/main/java/mkl/testarea/pdfbox1/extract/PrintPaths.java
@@ -12,6 +12,7 @@ import org.apache.pdfbox.pdfviewer.PageDrawer;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDResources;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.util.Matrix;
 import org.apache.pdfbox.util.TextPosition;
 
 /**
@@ -55,16 +56,16 @@ public class PrintPaths extends PageDrawer
     @Override
     public void fillPath(int windingRule) throws IOException
     {
-        System.out.println("Fill; windingrule: " + windingRule);
         printPath();
+        System.out.printf("Fill; windingrule: %s\n\n", windingRule);
         getLinePath().reset();
     }
 
     @Override
     public void strokePath() throws IOException
     {
-        System.out.println("Stroke");
         printPath();
+        System.out.printf("Stroke; unscaled width: %s\n\n", getGraphicsState().getLineWidth());
         getLinePath().reset();
     }
 
@@ -73,25 +74,48 @@ public class PrintPaths extends PageDrawer
         GeneralPath path = getLinePath();
         PathIterator pathIterator = path.getPathIterator(null);
 
+        double x = 0, y = 0;
         double coords[] = new double[6];
         while (!pathIterator.isDone()) {
             switch (pathIterator.currentSegment(coords)) {
             case PathIterator.SEG_MOVETO:
                 System.out.printf("Move to (%s %s)\n", coords[0], fixY(coords[1]));
+                x = coords[0];
+                y = coords[1];
                 break;
             case PathIterator.SEG_LINETO:
-                System.out.printf("Line to (%s %s)\n", coords[0], fixY(coords[1]));
+                double width = getEffectiveWidth(coords[0] - x, coords[1] - y);
+                System.out.printf("Line to (%s %s), scaled width %s\n", coords[0], fixY(coords[1]), width);
+                x = coords[0];
+                y = coords[1];
                 break;
             case PathIterator.SEG_QUADTO:
                 System.out.printf("Quad along (%s %s) and (%s %s)\n", coords[0], fixY(coords[1]), coords[2], fixY(coords[3]));
+                x = coords[2];
+                y = coords[3];
                 break;
             case PathIterator.SEG_CUBICTO:
                 System.out.printf("Cubic along (%s %s), (%s %s), and (%s %s)\n", coords[0], fixY(coords[1]), coords[2], fixY(coords[3]), coords[4], fixY(coords[5]));
+                x = coords[4];
+                y = coords[5];
                 break;
             case PathIterator.SEG_CLOSE:
                 System.out.println("Close path");
             }
             pathIterator.next();
         }
+    }
+    
+    double getEffectiveWidth(double dirX, double dirY)
+    {
+        if (dirX == 0 && dirY == 0)
+            return 0;
+        Matrix ctm = getGraphicsState().getCurrentTransformationMatrix();
+        double widthX = dirY;
+        double widthY = -dirX;
+        double widthXTransformed = widthX * ctm.getValue(0, 0) + widthY * ctm.getValue(1, 0);
+        double widthYTransformed = widthX * ctm.getValue(0, 1) + widthY * ctm.getValue(1, 1);
+        double factor = Math.sqrt((widthXTransformed*widthXTransformed + widthYTransformed*widthYTransformed) / (widthX*widthX + widthY*widthY));
+        return getGraphicsState().getLineWidth() * factor;
     }
 }
